ce705d87ab98b672e3942f599aaaa67095897317
Marcio Cavalcante <mca19ntl@gmail.com>
2021-12-03 09:46:26 -0300

    Backend project created

---
+++ backend/src/main/java/com/marciocazevedo/cltcrdapi/CltcrdapiApplication.java
@@ -0,0 +1,13 @@
+package com.marciocazevedo.cltcrdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CltcrdapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CltcrdapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/marciocazevedo/cltcrdapi/CltcrdapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marciocazevedo.cltcrdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CltcrdapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
